dd1bf03cf4f445b4190fec99959272cab28437fb
Tom Sell <thomas.j.sell@gmail.com>
2015-02-14 00:02:50 -0500

    added enemy, fixed annoying player clone bug

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,30 +3,34 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
-    NavMeshAgent nav;
+	Transform player;
+	//PlayerHealth playerHealth;
+	//EnemyHealth enemyHealth;
+	NavMeshAgent nav;
 
 
-    void Awake ()
-    {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <NavMeshAgent> ();
-    }
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Playa").transform;
+		//playerHealth = player.GetComponent <PlayerHealth> ();
+		//enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent> ();
+		Debug.Log (player);
+		Debug.Log (player.position);
+		Debug.Log (nav);
+	}
 
 
-    void Update ()
-    {
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
-    }
+	void Update ()
+	{
+		//if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		//{
+		nav.SetDestination (player.position);
+		Debug.Log ("Target postition: " + player.position);
+		//}
+		//else
+		//{
+		//    nav.enabled = false;
+		//}
+	}
 }
