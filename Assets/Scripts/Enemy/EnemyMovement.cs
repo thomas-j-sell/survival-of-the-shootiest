b9c3f2918945e5704c2ad6987895772cb7319542
Tom Sell <thomas.j.sell@gmail.com>
2015-02-16 13:32:20 -0500

    added combat scripts

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,33 +4,26 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour
 {
 	Transform player;
-	//PlayerHealth playerHealth;
-	//EnemyHealth enemyHealth;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
 	NavMeshAgent nav;
 
 
 	void Awake ()
 	{
 		player = GameObject.FindGameObjectWithTag ("Playa").transform;
-		//playerHealth = player.GetComponent <PlayerHealth> ();
-		//enemyHealth = GetComponent <EnemyHealth> ();
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent <EnemyHealth> ();
 		nav = GetComponent <NavMeshAgent> ();
-		Debug.Log (player);
-		Debug.Log (player.position);
-		Debug.Log (nav);
 	}
 
 
 	void Update ()
 	{
-		//if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-		//{
-		nav.SetDestination (player.position);
-		Debug.Log ("Target postition: " + player.position);
-		//}
-		//else
-		//{
-		//    nav.enabled = false;
-		//}
+		if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+			nav.SetDestination (player.position);
+		} else {
+			nav.enabled = false;
+		}
 	}
 }
